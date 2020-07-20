c4bb074f70895fb6b10a65a65a69f068dd47101e
Yash Gautam <35126822+yashgautam1221@users.noreply.github.com>
2020-07-20 23:55:35 +0530

    Update settings.py

--- Nykinsky/settings.py
+++ Nykinsky/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['yooggenykinsky.herokuapp.com', '*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
